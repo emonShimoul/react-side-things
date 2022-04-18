19991015e40a0ba2bacef2084b959d7e486d372c
emonShimoul <emonshimoul@gmail.com>
2022-04-18 11:52:09 +0600

    intro to local storage to store data

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { addToDb } from '../../utilities/fakedb';
 
 const Cosmetic = (props) => {
-    const {name, price} = props.cosmetic;
+    const {name, price, id} = props.cosmetic;
+    const handlePurchase = id => {
+        // set to localstorage
+        console.log(id);
+        addToDb(id);
+    }
     return (
         <div>
             <h2>Name: {name}</h2>
-            <p>Price: {price}</p>
+            <p>ID: {id} Price: {price}</p>
+            <button onClick={() => handlePurchase(id)}>Purchase</button>
         </div>
     );
 };
---
+++ src/utilities/fakedb.js
@@ -0,0 +1,13 @@
+// use local storage as your db for now
+const addToDb = id => {
+    const exists = localStorage.getItem(id);
+    if(!exists){
+        localStorage.setItem(id, 1);
+    }
+    else{
+        const newCount = parseInt(exists) + 1;
+        localStorage.setItem(id, newCount);
+    }
+}
+
+export {addToDb};
